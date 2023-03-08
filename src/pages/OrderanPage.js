44deb9a2c7fe439170bcb2bc8c404fb8c9995f4c
Andi Almaarif Arrozi <80453788+AndiAlmaarif69@users.noreply.github.com>
2023-03-08 07:42:04 +0700

    Update OrderanPage.js

--- src/pages/OrderanPage.js
+++ src/pages/OrderanPage.js
@@ -1,34 +1,19 @@
-// import React, { Component } from 'react';
-// import Table1 from '../Components/TableOrderan'
-
-// var data = [
-//   {id: 1, name: 'Gob', value: '2'},
-//   {id: 2, name: 'Buster', value: '5'},
-//   {id: 3, name: 'George Michael', value: '4'}
-// ];
-// class OrderanPage extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <p className="Table-header">Basic Table</p> 
-//         <Table1 data={data}/> 
-//       </div> 
-//     );
-//   }
-// }
-// export default OrderanPage;
-
 import React from 'react'
 import Navigation from '../Components/Navigation'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export default function OrderanPage() {
   return (
+    <>
+    <Navigation />
     <div className='orderan-bg'>
-        <Navigation />
-        <div className='judul-orderan'> 
-          <h1>Orderan</h1>
-        </div>
-        <div className="table-orderan">
+      <div style={{zIndex:-2}} class="orderskuy card position-absolute top-0 bottom-0 start-0 end-0 opacity-75 bg-light"></div>
+        <div className="table-orderan position-relative p-4">
+          <div className='judul-orderan'> 
+            <h1 className="text-center">Orderan</h1>
+          </div>
+          <div style={{zIndex:-1}} class="orderkuy card position-absolute top-0 bottom-0 start-0 end-0 opacity-50 bg-light"></div>
             <table>
                 <tr>
                     <th>No</th>
@@ -44,10 +29,24 @@ export default function OrderanPage() {
                     <td>Male</td>
                     <td>Malewfwegwegergerger</td>
                     <td>lkwegnlwkenglrn rjgn elrkgnlkrmg wemglkwerglkwnglknwlgnlwk erknglekrngklerg rkgmelk</td>
-                    <td>wefwe</td>
+                    <td>
+                      <DropdownButton
+                        variant="outline-success"
+                        title="Dropdown"
+                        id="input-group-dropdown-1"
+                      >
+                        <Dropdown.Item href="#">Action</Dropdown.Item>
+                        <Dropdown.Item href="#">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#">Separated link</Dropdown.Item>
+                      </DropdownButton>
+
+                    </td>
                 </tr>
             </table>
         </div>
     </div>
+    </>
   )
 }
